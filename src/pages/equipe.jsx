86c63fa7d0bd281895e976aea80dc4a8a8f5d38e
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-16 11:57:38 -0300

    Links do Next

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,19 @@
+import Menu from "@/components/Menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>Olá Next.Js!</h1>
+      <Menu />
+      <main>
+        <h2>Equipe</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius,
+          repellat fugiat eligendi necessitatibus nulla obcaecati deleniti ullam
+          laboriosam asperiores voluptatem natus numquam dignissimos quas hic
+          at! Adipisci animi odit aut.
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/servicos.jsx
@@ -0,0 +1,39 @@
+import Menu from "@/components/Menu";
+
+export default function Servicos() {
+  return (
+    <>
+      <h1>Olá Next.Js!</h1>
+      <Menu />
+      <main>
+        <h2>Serviços</h2>
+        <ul>
+          <li>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Architecto minima modi quia ex!
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Architecto minima modi quia ex!
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Architecto minima modi quia ex!
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Architecto minima modi quia ex!
+            </p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
+}
