763ae421c2df1561eeabc26e158f1dfa0ffa50e4
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-16 11:48:43 -0300

    criação do componente Menu

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Olá Next.Js!</h1>
+      <Menu />
+      <main>
+        <h2>Boas vindas ao Next.js</h2>
+        <h3>Página Inicial</h3>
+      </main>
+    </>
+  );
+}
