679aae956c7b0093194f15d38b8c2b02b2d3c8fb
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-17 09:10:36 -0300

    Heads e meta tags únicas para cada página

--- src/pages/equipe.jsx
+++ src/pages/equipe.jsx
@@ -1,19 +1,23 @@
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 export default function Equipe() {
   return (
     <>
+      <Head>
+        <title>Equipe - Meu app Next.js </title>
+        <meta name="description" content="Página de Equipe..." />
+      </Head>
       <h1>Olá Next.Js!</h1>
       <Menu />
-      <main>
-        <h2>Equipe</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius,
-          repellat fugiat eligendi necessitatibus nulla obcaecati deleniti ullam
-          laboriosam asperiores voluptatem natus numquam dignissimos quas hic
-          at! Adipisci animi odit aut.
-        </p>
-      </main>
+
+      <h2>Equipe</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, repellat
+        fugiat eligendi necessitatibus nulla obcaecati deleniti ullam laboriosam
+        asperiores voluptatem natus numquam dignissimos quas hic at! Adipisci
+        animi odit aut.
+      </p>
     </>
   );
 }
--- src/pages/servicos.jsx
+++ src/pages/servicos.jsx
@@ -1,39 +1,42 @@
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 export default function Servicos() {
   return (
     <>
+      <Head>
+        <title>Serviços - Meu app Next.js </title>
+        <meta name="description" content="Página de Serviços..." />
+      </Head>
       <h1>Olá Next.Js!</h1>
       <Menu />
-      <main>
-        <h2>Serviços</h2>
-        <ul>
-          <li>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Architecto minima modi quia ex!
-            </p>
-          </li>
-          <li>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Architecto minima modi quia ex!
-            </p>
-          </li>
-          <li>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Architecto minima modi quia ex!
-            </p>
-          </li>
-          <li>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Architecto minima modi quia ex!
-            </p>
-          </li>
-        </ul>
-      </main>
+      <h2>Serviços</h2>
+      <ul>
+        <li>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            minima modi quia ex!
+          </p>
+        </li>
+        <li>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            minima modi quia ex!
+          </p>
+        </li>
+        <li>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            minima modi quia ex!
+          </p>
+        </li>
+        <li>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            minima modi quia ex!
+          </p>
+        </li>
+      </ul>
     </>
   );
 }
